1c5bc750a6c63dfd7445e1a8e581abfc7aa664e5
Camila_Guevara <campguevara@gmail.com>
2026-03-02 10:01:59 -0500

    fix: corregir header roto en reportes.php, instanciar conn, centrar logos sidebar en main.css

--- api/reportes.php
+++ api/reportes.php
@@ -1,9 +1,12 @@
 <?php
-er('Content-Type: application/json');
+error_reporting(0);
+@ini_set('display_errors', 0);
+header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET');
 
 require_once __DIR__ . '/config/Database.php';
+$conn = Database::getInstance()->getConnection();
 
 function safeQuery($conn, $sql, $default = 0) {
     try {
