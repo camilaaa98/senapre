6968489e4bda29c4bdaabe47c87a023c5ac26853
Camila_Guevara <campguevara@gmail.com>
2026-03-02 18:43:46 -0500

    fix(css): eliminar ocultamiento del menu y restaurar logo a 100px con 50px radio. Actualizado v2.0.1

--- scripts/forzar_cache_css.php
+++ scripts/forzar_cache_css.php
@@ -3,7 +3,7 @@
  * scripts/forzar_cache_css.php
  * Añade/actualiza ?v= en todos los links de css/main.css en los HTML
  */
-$version  = '2.0.0';
+$version  = '2.0.1';
 $archivos = glob(__DIR__ . '/../*.html');
 $updated  = 0;
 
